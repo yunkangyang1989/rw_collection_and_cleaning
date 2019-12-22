959d485f233563031cc0414a0909d83540ccc859
Skorupski <r.skopruipski@sap.com>
2019-12-22 15:47:13 -0800

    method changes

---
+++ indicators.py
@@ -0,0 +1,28 @@
+indicators = [
+    'said', 
+    'added', 
+    'told', 
+    'blasted', 
+    'suggested', 
+    'claimed', 
+    'called', 
+    'conceded', 
+    'argued', 
+    'announced',    
+    'noted', 
+    'shouted', 
+    'testified', 
+    'rejected'
+]
+
+special_indicators = [
+    'according'
+]
+
+special_dict = {
+    'according': 'to'
+}
+
+exclude = [
+    'Fox', 
+    'News']

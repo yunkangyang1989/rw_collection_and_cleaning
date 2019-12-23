91e4b02a773d838be44e5f8f5f61d7e53a05e9ba
Yunkang Yang <40149391+yunkangyang1989@users.noreply.github.com>
2019-12-23 02:06:03 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,81 @@
+indicators = [
+    'said', 
+    'added', 
+    'told', 
+    'blasted', 
+    'suggested', 
+    'claimed', 
+    'called', 
+    'conceded', 
+    'argued', 
+    'announced',    
+    'noted', 
+    'shouted',
+    'acknowledged',
+    'admitted',
+    'agreed',
+    'alleged',
+    'asserted',
+    'believed',
+    'charged',
+    'claimed',
+    'complained',
+    'concluded',
+    'contended',
+    'criticized',
+    'declared',
+    'disagreed',
+    'disclosed',
+    'tweeted',
+    'estimated',
+    'explained',
+    'feared',
+    'hoped',
+    'insisted',
+    'maintained',
+    'mentioned',
+    'predicted',
+    'promised',
+    'recalled',
+    'recommended',
+    'replied',
+    'responded',
+    'reported',
+    'stated',
+    'stressed',
+    'emphasized',
+    'testified',
+    'thought',
+    'urged',
+    'warned',
+    'worried',
+    'wrote',
+    'observed',
+    'demanded',
+    'voiced',
+    'expressed',
+    'refuted',
+    'rebuffed',
+    'testified', 
+    'rejected'
+]
+
+special_indicators = [
+    'according'
+]
+
+special_dict = {
+    'according': 'to'
+}
+
+
+article1103 = 'UPDATE: The formerly anonymous accuser identified herself as Christine Blasey Ford, Fox News reported:\n\nThe letter was relayed to lawmakers by 51-year-old research psychologist Christine Blasey Ford and concerns an incident involving the 53-year-old Kavanaugh and her while they were in high school. According to two officials who spoke anonymously with the New York Times, the incident involved possible sexual misconduct between Kavanaugh and the woman.\n\nSenate Judiciary Committee Member John N. Kennedy said on "Fox News Sunday" that the Kavanaugh confirmation hearings have devolved into an "intergalactic freakshow."\n\n"I\'m fairly confident that our Founding Fathers did not intend the process to work this way," the Louisiana Republican told Chris Wallace.\n\nKennedy said Chairman Charles Grassley (R-Iowa) "did the best he could" to keep order during hearings marred by Democrats "ignoring rules" and about 240 protests by the senator\'s count.\n\n"The only thing missing was the genitalia-shaped headgear," Kennedy said of protesters, referring to the pink hats donned by anti-Trump demonstrators during the women\'s marches.\n\nI joined @FoxNewsSunday to talk about the latest developments with Judge Kavanaugh. Kavanaugh is extremely qualified to serve on the #SCOTUS. He has an outstanding and extensive judicial record and a strong judicial philosophy. https://t.co/MBOgroR0Sf — John Kennedy (@SenJohnKennedy) September 16, 2018\n\nKennedy blasted Sen. Dianne Feinstein (D-Calif.) for holding onto an anonymous letter alleging misconduct by Kavanaugh in high school. He also said the situation is one where many of the facts of the case cannot be reviewed by the members of the committee.\n\n"Most Americans... are looking at this and they\'re thinking that Congress has hit rock bottom and started to dig," he said.\n\nWallace asked Kennedy about incidents during the hearing involving Sens. Cory Booker (D-N.J.) and Kamala Harris (D-Calif.).\n\nSchiff: \'Bob Mueller Is the One Draining the Swamp\', Trump \'Terrified\' of Manafort\n\nPirro: To Dems Like Feinstein, \'You\'re Guilty Until Proven Innocent\'\n\nSen John Kennedy (R-LA) tells Chris that the Kavanaugh confirmation process so far has been an "intergalactic freak show." Watch the full interview on Fox News Channel at 2P/7P. https://t.co/lprtTBSKCk — FoxNewsSunday (@FoxNewsSunday) September 16, 2018\n\nBooker declared himself similar to "Spartacus" for willfully disobeying committee rules governing releases of documents. It was later determined the documents had been approved for release in some manner.\n\n"I did willingly violate the chairs rules on the committee\'s confidentiality process," Booker said at the time.\n\n.@SenJohnKennedy on the handling of the letter with allegations against Kavanaugh: They’ve had this stuff for three months. If they were serious about it, they should’ve told us about it. #KavanaughConfirmationHearings pic.twitter.com/YBXImQPoeL — FoxNewsSunday (@FoxNewsSunday) September 16, 2018\n\nKennedy said he was chairing the hearing in the temporary absence of Grassley, and refused to cut off Booker despite at least one plea from a Republican.\n\nI thought he should just "let it rip," Kennedy said of Booker\'s statements.\n\nHe said that Booker and Harris, who asked about Kavanaugh\'s possible relationship with private Trump attorney Marc Kasowitz\' firm, want liberal activist judges who "rewrite the Constitution every other Thursday."\n\nWatch more above.\n\nHuckabee: Kerry Talking to Iranians \'Makes the Case\' for First-Ever Logan Act Conviction\n\n\'Pretty Scummy\': \'The Five\' Blasts NYT for Now-Corrected Story on Haley\'s $52K Curtains'
+
+result1103 = ['two officials','John N. Kennedy','@FoxNewsSunday','Booker']
+
+article1104 = 'Two former girlfriends of Supreme Court nominee Brett Kavanaugh spoke out Monday on "The Story" in support of him, in the wake of sexual misconduct allegations.\n\nA California professor named Christine Blasey Ford was named as the formerly anonymous person in a report alleging Kavanaugh forced him self upon her at a party in Montgomery County, Md. in the 1980s.\n\nFord\'s account was brought to the Senate Judiciary Committee\'s attention by Ranking Member Dianne Feinstein (D-Calif.).\n\n.@mercedesschlapp: “Judge Kavanaugh is someone with impeccable credentials, someone who I know personally.” https://t.co/lgIXeBKVPz pic.twitter.com/du6idCXrUF — Fox News (@FoxNews) September 17, 2018\n\nMaura Fitzgerald and Maura Kane both joined 63 other women in signing a letter in support of Kavanaugh, Martha MacCallum reported.\n\nFitzgerald said she was "absolutely shocked" at the allegations against Kavanaugh and said they are "the polar opposite of the Brett Kavanaugh that I\'ve known for over 35 years."\n\nMcConnell: Why Did Dems Not Raise Kavanaugh Accuser Concerns During 65 Prior Meetings?\n\n\'Shame on Her\': Bruce Calls Out Feinstein for Using Kavanaugh Accuser\'s Letter as \'Political Hatchet\'\n\nKane said she also "couldn\'t believe" the allegations, adding that she attended the judge\'s public confirmation hearings and witnessed several other women offering statements supporting Kavanaugh.\n\nMacCallum said the situation reminds many people of how Anita Hill accused then-Supreme Court nominee Clarence Thomas of untoward conduct in the workplace.\n\n"All of the years that I\'ve known Brett, even if he had a beer, he was always in complete control," Fitzgerald said, nodding to the facet of the allegations that said Kavanaugh was intoxicated at the time of the purported incident.\n\nKane said that she drank socially with Kavanaugh while they were in high school, adding that the drinking age in the District of Columbia was 18 at the time.\n\n"He never became \'someone different\' after drinking," she said.\n\nWatch more above.\n\nBREAKING: Senate panel invites Kavanaugh, accuser to public hearing; Thursday vote delayed, sources say. https://t.co/aan2O2eCXp — Fox News (@FoxNews) September 17, 2018\n\nProof of \'Corruption\' or a \'Flailing President\': The Five Reacts to Trump Declassifying FISA Docs\n\nBret Baier: Kavanaugh Allegation \'Politically Dangerous\' for Both Parties'
+
+result1104 = ['@FoxNews','Fitzgerald','Kane','MacCallum']
+
+
+
